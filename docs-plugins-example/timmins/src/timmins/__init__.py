5bc44861956594ebdb51580a2b99edbd9434204c
ftnext <takuyafjp+develop@gmail.com>
2023-07-01 11:50:24 +0900

    [feat] timmins supports plugins

--- docs-plugins-example/timmins/src/timmins/__init__.py
+++ docs-plugins-example/timmins/src/timmins/__init__.py
@@ -1,5 +1,12 @@
-def display(text: str) -> None:
-    print(text)
+from importlib.metadata import entry_points
+
+display_eps = entry_points(group="timmins.display")
+try:
+    display = display_eps[0].load()
+except IndexError:
+
+    def display(text: str) -> None:
+        print(text)
 
 
 def hello_world() -> None:
